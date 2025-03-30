b798cfa8437545b6860fa7030a64e8b23bc4fb0e
Sadaf-dll777 <md.saifalsadaf@gmail.com>
2025-03-30 10:38:37 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+
+
+module.exports = {
+  TOKEN: "",
+  language: "en",
+  ownerID: ["1004206704994566164", ""], 
+  mongodbUri : "mongodb+srv://shiva:shiva@discordbot.mlasw.mongodb.net/?retryWrites=true&w=majority",
+  setupFilePath: './commands/setup.json',
+  commandsDir: './commands',  
+  embedColor: "#1db954",
+  activityName: "YouTube Music", 
+  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  embedTimeout: 5, 
+  errorLog: "", 
+  nodes: [
+{
+      name: "Avinan",
+      password: "avinan",
+      host: "new-york-node-1.vortexcloud.xyz",
+      port:  5008,
+      secure: false
+}
+  ]
+}
