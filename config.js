a95d6638265819ab5b0d22e42d24744de34ee360
Sadaf-dll777 <md.saifalsadaf@gmail.com>
2025-05-09 15:04:17 +0600

    Update config.js

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
 {
-      name: "dj wale babu",
-      password: "inosuke",
-      host: "inosuke.69.mu",
-      port:  11622,
+      name: "GlaceYT",
+      password: "glaceyt",
+      host: "193.226.78.187",
+      port:  3543,
       secure: false
     }
   ]
